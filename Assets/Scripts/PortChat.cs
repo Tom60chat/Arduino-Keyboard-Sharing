6d8ccabed6fe22222d903d78f753858680ddc8fb
Tom Olivier <Tom602001@live.fr>
2022-10-17 14:47:42 +0200

    First commit

---
+++ Assets/Scripts/PortChat.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Input;
+
+namespace ArduinoKeyboard
+{
+    class PortChat
+    {
+        static bool _continue;
+        static SerialPort _serialPort;
+        public static Action<string> Output = (m) => { Console.WriteLine(m); };
+
+        public static void Start(string Port)
+        {
+            StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+            Thread readThread = new Thread(Read);
+            Thread writeThread = new Thread(Write);
+
+            // Create a new SerialPort object with default settings.
+            _serialPort = new SerialPort();
+
+            // Allow the user to set the appropriate properties.
+            _serialPort.PortName = Port;
+            /*_serialPort.BaudRate = ;
+            _serialPort.Parity = ;
+            _serialPort.DataBits = ;
+            _serialPort.StopBits = ;
+            _serialPort.Handshake = ;*/
+
+            // Set the read/write timeouts
+            _serialPort.ReadTimeout = 2000;
+            //_serialPort.WriteTimeout = 2000;
+
+            Output.Invoke("Open serial: " + _serialPort.PortName);
+            _serialPort.Open();
+            _continue = true;
+            //readThread.Start();
+            writeThread.Start();
+
+            //readThread.Join();
+            writeThread.Join();
+            Output.Invoke("Close serial");
+            _serialPort.Close();
+        }
+
+        private static void Read()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    string message = _serialPort.ReadLine();
+                    Output.Invoke(message);
+                }
+                catch (TimeoutException)
+                {
+                    Output.Invoke("Read Timeout"); ;
+                }
+            }
+        }
+
+        private static void Write()
+        {
+            Random random = new Random();
+            List<Key> Old_keysDown = new List<Key>();
+            while (_continue)
+            {
+                Thread.Sleep(50);
+                try
+                {
+                    List<Key> keysDown = new List<Key>();
+                    List<Key> keysUp = new List<Key>();
+                    System.Windows.Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        foreach (Key key in Enum.GetValues(typeof(Key)))
+                            if (key != Key.None)
+                                if (Keyboard.IsKeyDown(key))
+                                        keysDown.Add(key);
+                    });
+
+                    foreach (Key Key in Old_keysDown)
+                    {
+                        if (!keysDown.Contains(Key))
+                            keysUp.Add(Key);
+                    }
+
+                    string message = "";
+                    foreach (Key key in keysDown)
+                        message += "Down:" + key + "&";
+                    foreach (Key key in keysUp)
+                        message += "Up:" + key + "&";
+                    if (message != "" && _serialPort.IsOpen)
+                    {
+                        _serialPort.Write(message);
+                        Output.Invoke(message);
+                    }
+                    Old_keysDown = keysDown;
+                }
+                catch (TimeoutException)
+                {
+                    Output.Invoke("Write Timeout"); ;
+                }
+            }
+        }
+
+        public static void Stop()
+        {
+            Output.Invoke("Stop"); ;
+            _continue = false;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArduinoKeyboard
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Refresh_Port();
+
+            PortChat.Output += (s) =>
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    Output_TextBox.Text += s + "\n";
+                });
+            };
+        }
+
+        string[] FetchingArduino()
+        {
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+
+            Console.WriteLine("The following serial ports were found:");
+
+            // Display each port name to the console.
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+            }
+
+            Console.ReadLine();
+            return ports;
+        }
+
+        BackgroundWorker worker = new BackgroundWorker();
+
+        void Arduino(string Port)
+        {
+            if (Port == null || Port == "")
+                return;
+
+            if (worker.IsBusy)
+            {
+                PortChat.Stop();
+                Start_Button.Content = "Start";
+                return;
+            }
+
+            worker.DoWork += (s, e) =>
+            {
+                PortChat.Start(Port);
+            };
+
+            Start_Button.Content = "Stop";
+            worker.RunWorkerAsync();
+        }
+
+        void Refresh_Port()
+        {
+            Port_ComboBox.Items.Clear();
+            foreach (string port in FetchingArduino())
+                Port_ComboBox.Items.Add(port);
+
+            if (Port_ComboBox.Items.Count != 0)
+            {
+                Start_Button.Visibility = Visibility.Visible;
+                Port_ComboBox.SelectedItem = Port_ComboBox.Items[0];
+            }
+            else
+                Start_Button.Visibility = Visibility.Hidden;
+        }
+
+        private void Refresh(object sender, RoutedEventArgs e)
+        {
+            Refresh_Port();
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            if(Port_ComboBox.SelectedItem != null)
+                Arduino(Port_ComboBox.SelectedItem.ToString());
+        }
+    }
+}
